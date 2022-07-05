8f480a553f0367bd752d4b10bc5c5b6b04b3650d
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-07-05 16:55:17 -0700

    Move code to Account class + save new account

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -1,12 +1,54 @@
 class Account {
   constructor(username) {
-    this.username = username;
-    this.transactions = [];
+    this.username = username
+    this.transactions = []
+  }
+
+  validate(accounts) {
+    let validationMsg = ''
+
+    if (!this.username) {
+      validationMsg = 'Please enter a value'
+    }
+
+    const alreadyExists = accounts.some((a) => a.username === this.username)
+
+    if (alreadyExists) {
+      validationMsg = `There is already an account with the name "${a.username}". Please, try a different one`
+    }
+
+    return validationMsg
+  }
+
+  save() {
+    const newAccount = {
+      username: this.username,
+      transactions: [],
+    }
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: 'POST',
+        url: `${BASE_ENDPOINT}/accounts`,
+        data: JSON.stringify({ newAccount }),
+        dataType: 'json',
+        contentType: 'application/json',
+      })
+        .done((data) => resolve(data))
+        .fail((err) => reject(err))
+    })
   }
 
   get balance() {
     return this.transactions.reduce((total, transaction) => {
-      return total + transaction;
-    }, 0);
+      return total + transaction
+    }, 0)
+  }
+
+  static getAccounts() {
+    return new Promise((resolve, reject) => {
+      $.ajax(`${BASE_ENDPOINT}/accounts`)
+        .done((data) => resolve(data))
+        .fail((err) => reject(err))
+    })
   }
 }
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,36 +2,14 @@ $(() => {
   $('#account-form').on('submit', async (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
-    const newAccount = new Account(formData.username)
-    const accounts = await getAccounts()
-    const validationMsg = validateAccount(newAccount, accounts)
+    const newAccount = new Account(formData.get('username'))
+    const accounts = await Account.getAccounts()
+    const validationMsg = newAccount.validate(accounts)
 
     if (validationMsg) {
       alert(validationMsg)
+    } else {
+      newAccount.save()
     }
   })
 })
-
-function getAccounts() {
-  return new Promise((resolve, reject) => {
-    $.ajax(`${BASE_ENDPOINT}/accounts`)
-      .done((data) => resolve(data))
-      .fail((err) => reject(err))
-  })
-}
-
-function validateAccount(account, accounts) {
-  let validationMsg = ''
-
-  if (!account.username) {
-    validationMsg = 'Please enter a value'
-  }
-
-  const alreadyExists = accounts.some((a) => a.username === account.username)
-
-  if (alreadyExists) {
-    validationMsg = `There is already an account with the name "${a.username}". Please, try a different one`
-  }
-
-  return validationMsg
-}
