b38ae51485bfb47731ced6e288c07b3c622a3868
Mauricio Tomaz <mauriciotomazz@gmail.com>
2022-07-04 17:04:29 -0700

    Implement account form and validation

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,37 @@
 $(() => {
-  //Start coding here!
-});
+  $('#account-form').on('submit', async (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const newAccount = new Account(formData.username)
+    const accounts = await getAccounts()
+    const validationMsg = validateAccount(newAccount, accounts)
+
+    if (validationMsg) {
+      alert(validationMsg)
+    }
+  })
+})
+
+function getAccounts() {
+  return new Promise((resolve, reject) => {
+    $.ajax(`${BASE_ENDPOINT}/accounts`)
+      .done((data) => resolve(data))
+      .fail((err) => reject(err))
+  })
+}
+
+function validateAccount(account, accounts) {
+  let validationMsg = ''
+
+  if (!account.username) {
+    validationMsg = 'Please enter a value'
+  }
+
+  const alreadyExists = accounts.some((a) => a.username === account.username)
+
+  if (alreadyExists) {
+    validationMsg = `There is already an account with the name "${a.username}". Please, try a different one`
+  }
+
+  return validationMsg
+}
